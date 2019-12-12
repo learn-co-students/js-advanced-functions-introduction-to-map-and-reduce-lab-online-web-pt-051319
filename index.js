cf5f2a56389497c12bea9fcb753ef7fdb4ddc282
Kelley Chaplain <kmchaplain@gmail.com>
2019-12-12 09:21:13 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+const mapToNegativize = function(arr) {
+  const negArr = []
+  arr.forEach(n => negArr.push(n * (-1)))
+  return negArr
+};
+
+const mapToNoChange = function(arr) {
+  const newArr = []
+  arr.forEach(n => newArr.push(n))
+  return newArr
+};
+
+const mapToDouble = function(arr) {
+  const newArr = []
+  arr.forEach(n => newArr.push(n * 2))
+  return newArr
+};
+
+const mapToSquare = function(arr) {
+  const newArr = []
+  arr.forEach(n => newArr.push(n * n))
+  return newArr
+};
+
+const reduceToTotal = function(arr, start=0) {
+  const collectArr = start
+  arr.forEach(n => collectArr += n )
+  return collectArr
+};
