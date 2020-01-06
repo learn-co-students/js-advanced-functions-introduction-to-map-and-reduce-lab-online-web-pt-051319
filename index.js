13ceb37d06a6edfd731fbab68294c375be6bd524
lexus2727 <lexus2424@yahoo.com>
2020-01-06 11:31:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+    return sourceArray.map((e) => {return e * -1})
+  }
+
+  function mapToNoChange(dune) {
+      return dune.map((e) => {return e })
+  }
+
+  function mapToDouble(sourceArray) {
+      return sourceArray.map((e) => {return e * 2})
+  }
+
+  function mapToSquare(sourceArray) {
+      return sourceArray.map((e) => { return e * e})
+  }
+
+  function reduceToTotal(sourceArray, startingPoint = 0) {
+    return sourceArray.reduce((t, e) => {return t + e}, startingPoint);
+  }
+
+  function reduceToAllTrue(sourceArray) {
+    return sourceArray.reduce((t, e) => {return t && !!e}, true);
+  }
+
+  function reduceToAnyTrue(sourceArray) {
+    return sourceArray.reduce((t, e) => {return t || !!e}, false);
+  }
+  
+
